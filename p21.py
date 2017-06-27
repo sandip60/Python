3b3958695dcaa30c8ef7e042507c6452ba483882
sandip60 <sk8957953@gmail.com>
2017-06-28 02:08:56 +0530

    Second day  code

---
+++ p21.py
@@ -0,0 +1,135 @@
+
+#varibles
+name ="sandip kumar ";
+name="sher";
+name ="kumar sandip ";
+print(name);
+
+
+#data type
+
+#number
+num1 =30 ;
+print(type(num1));
+print(num1);
+
+num1 = 30.567;
+print(type(num1));
+print (num1);
+
+num1 = 1 + 7j ;
+print(type(num1));
+print(num1);
+print(num1.real);
+print(num1.imag);
+
+#string
+num1 = "Sandip";
+print(type(num1));
+print (num1);
+
+num1 = '300' ;
+print(type(num1));
+print (num1);
+
+#list
+num1 = ["Sandip" , 40 , 40.2 ];
+print(type(num1[0]));
+print(num1[0]);
+print(type(num1));
+print (num1);
+num1[2]=354;
+print(num1);
+
+
+#tuples
+S= ('s',20,20.6,1+8j);
+P=('p',30);
+print(S);
+print(type(S));
+
+print(S[1]);
+print(S[-1].imag);
+print(S[2:]);
+print(S[1:3]);
+print(P*2);
+print(P*2+S);
+
+#Dictionary
+
+s = {'nameFirst':'sandip','nameLast':"Kuar",'age':20};
+print(s);
+print(s.keys());
+print(s.values());
+
+#type coversion/type casting
+a = 12.45
+print(type(a));
+b = int (a);
+print(type(b));
+print(b);
+
+
+#operators
+a=2;
+b =3 ;
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a%b);
+print(a**b);
+
+
+
+
+
+
+#shersin9891@GMAIL.COM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
